65f20cdf30aaaa420eb65049d45ca60229a677eb
Vichayuth Tavichaiyuth <56262507+Vichayuth@users.noreply.github.com>
2022-05-10 15:29:20 +0700

    setup-backend

---
+++ backend/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Quote = require('inspirational-quotes')
+const cors = require('cors')
+const port = 3001
+const mongodb = require('mongodb');
+const { json } = require('express/lib/response');
+const uri = "mongodb+srv://vichayuth:pruk2543@cluster0.agzry.mongodb.net/mydb?retryWrites=true&w=majority"
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+app.listen(port, () => {
+    console.log(`Server started successfully at port ${port}`)
+})
+
+
+app.post('/users/create', async (req, res) => {
+    const user = req.body;
+    console.log(user)
+    const client = new mongodb.MongoClient(uri);
+    await client.connect();
+    await client.db("mydb").collection("users").insertOne({
+        id: parseInt(user.id),
+        fname: user.fname,
+        lname: user.lname,
+        username: user.username,
+        email: user.email,
+        avatar: user.avatar
+    });
+    await client.close();
+    res.status(200).send({
+        "status": "ok",
+        "message": "User with ID" + user.id + "is created",
+        "user":user
+    }) 
+
+})
+
+app.get('/users', async (req, res) => {
+    const client = new mongodb.MongoClient(uri);
+    await client.connect();
+    const users = await client.db("mydb").collection("users").find({}).toArray()
+    await client.close();
+    res.status(200).send(users)
+})
+
+app.delete('/users/delete', async (req, res) => {
+    const users = req.body;
+    console.log(users)
+    const client = new mongodb.MongoClient(uri);
+    await client.connect();
+    await client.db("mydb").collection("users").deleteOne({
+        id: parseInt(users.id),
+
+    });
+    await client.close();
+    res.status(200).send({
+        "status": "ok",
+        "message": "User with ID" + users.id + "is created",
+        "user":users
+    }) 
+
+})
